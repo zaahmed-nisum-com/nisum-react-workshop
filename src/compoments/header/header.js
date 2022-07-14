1c2273fd183a3d1225d5f4a2c3c7f1552926a6b6
Zain Ahmed <zain.ahmed@backcountry.com>
2022-07-14 15:11:09 +0500

    update

---
+++ src/compoments/header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(props) {
+  const handleFN = () => {};
+
+  return (
+    <div>
+      <p className="heading-font-size">Header</p>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../compoments/header/header'
+
+function About(props) {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+}
+
+export default About;
